e0bb63c7957d16e42f8c1cd1531b81510f708afb
Felipe Vera <felipevera10@gmail.com>
2024-06-03 03:08:20 -0400

    first commit

---
+++ pages/contact/index.vue
@@ -0,0 +1,6 @@
+<template>
+   <div>
+      <h1>Contact Page</h1>
+   </div>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+   <div>
+      <h1>Home</h1>
+   </div>
+</template>
+
+<style>
+* {
+   margin: 0;
+   padding: 0;
+}
+</style>
